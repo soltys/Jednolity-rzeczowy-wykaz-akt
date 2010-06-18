1a8206a11e3402d8e3e1442fa2958379ada6cd68
soltys <soltys@uznam.org>
2010-06-18 18:47:08 +0200

    Created class-struct SymbolInfo

---
+++ src/com/urzad/util/SymbolInfo.java
@@ -0,0 +1,8 @@
+package com.urzad.util;
+public class SymbolInfo {
+	public String[] columns = new String [5];
+	public String name;
+	public String ka_km;
+	public String kA_ki;
+	public String comments;
+}
