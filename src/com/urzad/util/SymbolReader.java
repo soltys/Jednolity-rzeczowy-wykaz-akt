79f12d3f640b20bcf91bb394ccd714ed34f0603c
soltys <soltys@uznam.org>
2010-06-25 11:59:54 +0200

    cleaned import

--- src/com/urzad/util/SymbolReader.java
+++ src/com/urzad/util/SymbolReader.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 public class SymbolReader {
 	private String filename;
 	
